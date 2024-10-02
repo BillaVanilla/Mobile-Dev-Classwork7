8f96917677d5f5c4645edf5436c2be50564e3150
BillaVanilla <132112691+BillaVanilla@users.noreply.github.com>
2024-10-02 19:03:19 -0400

    Added RotationTransition, Removed Text, added initState,dispose, and speed toggle

--- main.dart
+++ main.dart
@@ -18,7 +18,7 @@ _SonicAnimationState createState() => _SonicAnimationState();
 class _SonicAnimationState extends State<SonicAnimation> with SingleTickerProviderStateMixin{
 bool _isGoingFast = true;
 bool _isVisible = true;
-AnimationController _animationController; 
+late AnimationController _animationController; 
 
 
 void initState(){
@@ -40,6 +40,19 @@ setState(() {
 _isVisible = !_isVisible;
   });
 }
+
+ void toggleSpeed() {
+    setState(() {
+      _isGoingFast = !_isGoingFast;
+      if (_isGoingFast) {
+        _animationController.duration = Duration(seconds: 1); // Sonic is going at a Fast speed
+      } else {
+        _animationController.duration = Duration(seconds: 8); // Sonic is going at a Slow speed
+      }
+      _animationController.repeat(); // Restart animation
+    });
+  }
+
 @override
 Widget build(BuildContext context) {
 return Scaffold(
@@ -50,16 +63,34 @@ body: Center(
 child: AnimatedOpacity(
 opacity: _isVisible ? 1.0 : 0.0,
 duration: Duration(seconds: 3),
-child: Image.network(
-'Sonic Adventures.png',
+child: RotationTransition(
+            turns: _animationController.drive(Tween(begin: 0.0, end: 1.0)),
+            child: Image.network(
+              'sonic_adventures.png', // Use a valid URL
+        ),
     ),
   ),
 ),
-floatingActionButton: FloatingActionButton(
-onPressed: toggleVisibility,
-child: Icon(Icons.play_arrow),
-backgroundColor: Colors.red,
+floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: toggleVisibility,
+            child: Icon(Icons.visibility),
+            backgroundColor: Colors.red,
+            heroTag: "toggleVisibility",
+          ),
+          
+          SizedBox(height: 20),
+          FloatingActionButton(
+            onPressed: toggleSpeed,
+            child: Icon(Icons.speed),
+            backgroundColor: Colors.blue,
+            heroTag: "Sonic Go Fast!!!",
+          ),
+        ],
       ),
     );
   }
 }
+
